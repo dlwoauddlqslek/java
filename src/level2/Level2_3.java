a45726dd7a4f823f5fb6aaba2c2a344a1ee95dbe
dlwoauddlqslek <ljm5857@gmail.com>
2024-06-23 04:08:32 +0900

    Initial commit

---
+++ src/level2/Level2_3.java
@@ -0,0 +1,7 @@
+package level2;
+
+public class Level2_3 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/level2/Level2_4.java
@@ -0,0 +1,76 @@
+package level2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Level2_4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("가위 바위 보 선택 : "); String player = scan.next();
+        int random = new Random().nextInt(3);
+
+        String com;
+        if(random==0){com="가위";}
+        else if(random==1){com="바위";}
+        else{com="보";}
+
+        if(player.equals("가위")){
+            if(com.equals("가위")){
+                System.out.println("comran) 가위");
+                System.out.println("result) 무승부");
+                return;
+            }
+            if(com.equals("바위")){
+                System.out.println("comran) 바위");
+                System.out.println("result) com 승리");
+                return;
+            }
+            if(com.equals("보")){
+                System.out.println("comran) 보");
+                System.out.println("result) player 승리");
+                return;
+            }
+
+        }
+
+        if(player.equals("바위")){
+            if(com.equals("가위")){
+                System.out.println("comran) 가위");
+                System.out.println("result) player 승리");
+                return;
+            }
+            if(com.equals("바위")){
+                System.out.println("comran) 바위");
+                System.out.println("result) 무승부");
+                return;
+            }
+            if(com.equals("보")){
+                System.out.println("comran) 보");
+                System.out.println("result) com 승리");
+                return;
+            }
+
+        }
+
+        if(player.equals("보")){
+            if(com.equals("가위")){
+                System.out.println("comran) 가위");
+                System.out.println("result) com 승리");
+                return;
+            }
+            if(com.equals("바위")){
+                System.out.println("comran) 바위");
+                System.out.println("result) player 승리");
+                return;
+            }
+            if(com.equals("보")){
+                System.out.println("comran) 보");
+                System.out.println("result) 무승부");
+                return;
+            }
+
+        }
+
+    }
+}
